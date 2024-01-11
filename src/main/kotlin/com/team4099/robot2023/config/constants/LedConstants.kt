c055019595a32ab88a6e969b9b72ac3b12e41c6e
CodingMaster121 <ytryanlo@gmail.com>
2024-01-10 20:44:41 -0500

    Added LED states

--- src/main/kotlin/com/team4099/robot2023/config/constants/LedConstants.kt
+++ src/main/kotlin/com/team4099/robot2023/config/constants/LedConstants.kt
@@ -1,7 +1,17 @@
 package com.team4099.robot2023.config.constants
 
 object LedConstants {
+    // TODO: Review states and change them if needed
     enum class LEDMode {
-        IDLE
+        IDLE,
+        AUTO,
+        INTAKE,
+        OUTTAKE,
+        TELEOP,
+        NOTE,
+        SOURCE,
+        MOVEMENT,
+        SCORE_SPEAKER,
+        SCORE_AMP
     }
 }
