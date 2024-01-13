14b7e897c637ef3060ee27515473e0533c1b4340
SirBeans <146898838+SirBeans@users.noreply.github.com>
2024-01-12 21:22:36 -0500

    added feeder and wrist and did requests and started states

--- src/main/kotlin/com/team4099/robot2023/subsystems/Shooter/Shooter.kt
+++ src/main/kotlin/com/team4099/robot2023/subsystems/Shooter/Shooter.kt
@@ -1,3 +1,96 @@
 package com.team4099.robot2023.subsystems.Shooter
 
+<<<<<<< HEAD
 class Shooter
+=======
+import com.team4099.lib.hal.Clock
+import com.team4099.lib.logging.LoggedTunableValue
+import com.team4099.robot2023.subsystems.drivetrain.drive.Drivetrain.Companion.fromRequestToState
+import com.team4099.robot2023.subsystems.superstructure.Request
+import edu.wpi.first.units.Voltage
+import org.team4099.lib.controller.SimpleMotorFeedforward
+import org.team4099.lib.units.base.Meter
+import org.team4099.lib.units.base.seconds
+import org.team4099.lib.units.derived.*
+
+class Shooter (val io: ShooterIO){
+    val inputs = ShooterIO.ShooterIOInputs()
+    private var RollerFeedforward: SimpleMotorFeedforward<Meter, Volt>
+    private var WristFeedforward: SimpleMotorFeedforward<Meter, Volt>
+
+
+    private val wristRollerkP =
+        LoggedTunableValue("Wrist/kP", Pair({ it.inVoltsPerInch }, { it.volts.perInch }))
+    private val wristRollerkI =
+        LoggedTunableValue(
+            "Wrist/kI", Pair({ it.inVoltsPerInchSeconds }, { it.volts.perInchSeconds })
+        )
+    private val wristRollerkD =
+        LoggedTunableValue(
+            "wrist/kD", Pair({ it.inVoltsPerInchPerSecond }, { it.volts.perInchPerSecond })
+        )
+    var currentState = ShooterStates.UNINITIALIZED
+    var rollerTargetVoltage : ElectricalPotential= 0.0.volts
+    var wristTargetVoltage : ElectricalPotential = 0.0.volts
+    var feederTargetVoltage : ElectricalPotential = 0.0.volts
+    fun setRollerVoltage(appliedVoltage: ElectricalPotential){
+        io.setRollerVoltage(appliedVoltage)
+    }
+    fun setWristVoltage(appliedVoltage: ElectricalPotential){
+        io.setWristVoltage(appliedVoltage)
+    }
+    fun setFeederVoltage(appliedVoltage: ElectricalPotential){
+        io.setFeederVoltage(appliedVoltage)
+    }
+    var lastRollerRunTime = 0.0.seconds
+    var lastWristRunTime = 0.0.seconds
+    var lastFeederRunTime = 0.0.seconds
+    var isZeroed : Boolean = false
+    var currentRequest = Request.ShooterRequest.OpenLoop
+fun periodic(){
+    io.updateInputs(inputs)
+    var nextState = currentState
+    when (currentState) {
+        ShooterStates.UNINITIALIZED -> {
+            nextState = ShooterStates.ZERO
+        }
+        ShooterStates.ZERO ->{
+//TODO write zero function for shooter
+        }
+        ShooterStates.IDLE ->{
+//TODO figure out if were
+        }
+        ShooterStates.OPEN_LOOP ->{
+            setRollerVoltage(rollerTargetVoltage)
+            lastRollerRunTime = Clock.fpgaTime
+
+            setWristVoltage(wristTargetVoltage)
+            lastWristRunTime = Clock.fpgaTime
+
+            setFeederVoltage(feederTargetVoltage)
+            lastFeederRunTime = Clock.fpgaTime
+            if (isZeroed == true ){
+                nextState = fromRequestToState(currentRequest)
+            }
+
+        }
+
+        ShooterStates.TARGETING_POSITION ->{
+
+
+        }
+
+    }
+
+}
+
+    companion object {
+        enum class ShooterStates{
+            UNINITIALIZED,
+            ZERO,
+            OPEN_LOOP,
+            TARGETING_POSITION,
+        }
+    }
+}
+>>>>>>> ec012fa (added feeder and wrist and did requests and started states)
--- src/main/kotlin/com/team4099/robot2023/subsystems/Shooter/ShooterIO.kt
+++ src/main/kotlin/com/team4099/robot2023/subsystems/Shooter/ShooterIO.kt
@@ -16,48 +16,121 @@ interface ShooterIO {
         var rollerSupplyCurrent = 0.amps
         var rollerTempreature = 0.celsius
 
+        var wristPostion = 0.degrees
+        var wristAppliedVoltage = 0.volts
+        var wristStatorCurrent = 0.amps
+        var wristSupplyCurrent = 0.amps
+        var wristTemperature = 0.celsius
+
+        var feederVelocity = 0.rotations.perMinute
+        var feederAppliedVoltage = 0.volts
+        var feederStatorCurrent = 0.amps
+        var feederSupplyCurrent = 0.amps
+        var feederTemperature = 0.celsius
+
         override fun toLog(table: LogTable) {
-            table?.put("rollerVelocityRPM", rollerVelocity.inRadiansPerSecond)
-            table?.put("rollerAppliedVoltage", rollerAppliedVoltage.inVolts)
-            table?.put("rollerStatorCurrent", rollerStatorCurrent.inAmperes)
-            table?.put("rollerSupplyCurrent", rollerSupplyCurrent.inAmperes)
-            table?.put("rollerTempreature", rollerTempreature.inCelsius)
+            table.put("rollerVelocityRPM", rollerVelocity.inRadiansPerSecond)
+            table.put("rollerAppliedVoltage", rollerAppliedVoltage.inVolts)
+            table.put("rollerStatorCurrent", rollerStatorCurrent.inAmperes)
+            table.put("rollerSupplyCurrent", rollerSupplyCurrent.inAmperes)
+            table.put("rollerTempreature", rollerTempreature.inCelsius)
+
+            table.put("wristPostion", wristPostion.inDegrees)
+            table.put("wristAppliedVoltage", wristAppliedVoltage.inVolts)
+            table.put("wristStatorCurrent", wristStatorCurrent.inAmperes)
+            table.put("wristSupplyCurrent", wristSupplyCurrent.inAmperes)
+            table.put("wristTemperature", wristTemperature.inCelsius)
+
+            table.put("feederVelocity", feederVelocity.inRadiansPerSecond)
+            table.put("feederAppliedVoltage", feederAppliedVoltage.inVolts)
+            table.put("feederStatorCurrent", feederStatorCurrent.inAmperes)
+            table.put("feederSupplyCurrent", feederSupplyCurrent.inAmperes)
+            table.put("feederTemperature", feederTemperature.inCelsius)
 
         }
 
         override fun fromLog(table: LogTable) {
-            table?.get("rollerVelocityRPM", rollerVelocity.inRadiansPerSecond)?.let {
+            //roller logs
+            table.get("rollerVelocityRPM", rollerVelocity.inRadiansPerSecond).let {
                 rollerVelocity = it.radians.perSecond
             }
-            table?.get("rollerAppliedVoltage", rollerAppliedVoltage.inVolts)?.let {
+            table.get("rollerAppliedVoltage", rollerAppliedVoltage.inVolts).let {
                 rollerAppliedVoltage = it.volts
             }
-            table?.get("rollerStatorCurrent", rollerStatorCurrent.inAmperes)?.let {
+            table.get("rollerStatorCurrent", rollerStatorCurrent.inAmperes).let {
                 rollerStatorCurrent = it.amps
             }
-            table?.get("rollerSupplyCurrent", rollerSupplyCurrent.inAmperes)?.let {
+            table.get("rollerSupplyCurrent", rollerSupplyCurrent.inAmperes).let {
                 rollerSupplyCurrent = it.amps
 
             }
-
-            table?.get("rollerTempreature", rollerTempreature.inCelsius)?.let {
+            table.get("rollerTempreature", rollerTempreature.inCelsius).let {
                 rollerTempreature = it.celsius
             }
+//wrist logs
+            table.get("wristPostion", wristPostion.inDegrees).let {
+                wristPostion = it.degrees
+            }
+            table.get("wristAppliedVoltage", wristAppliedVoltage.inVolts).let {
+                wristAppliedVoltage = it.volts
+            }
+            table.get("wristStatorCurrent", wristStatorCurrent.inAmperes).let {
+                wristStatorCurrent = it.amps
+            }
+            table.get("wristSupplyCurrent", wristSupplyCurrent.inAmperes).let {
+                wristSupplyCurrent = it.amps
 
+            }
+            table.get("wristTemperature", wristTemperature.inCelsius).let {
+                wristTemperature = it.celsius
+            }
+            //feeder
+            table.get("feederVelocity", feederVelocity.inRadiansPerSecond).let {
+                feederVelocity = it.radians.perSecond
+            }
+            table.get("feederAppliedVoltage", feederAppliedVoltage.inVolts).let {
+                feederAppliedVoltage = it.volts
+            }
+            table.get("feederStatorCurrent", feederStatorCurrent.inAmperes).let {
+                feederStatorCurrent = it.amps
+            }
+            table.get("feederSupplyCurrent", feederSupplyCurrent.inAmperes).let {
+                feederSupplyCurrent = it.amps
+
+            }
+            table.get("feederTemperature", feederTemperature.inCelsius).let {
+                feederTemperature = it.celsius
 
         }
     }
 
+
+}
+
     fun updateInputs(inputs: ShooterIOInputs){
 
     }
-    fun setRollerPower (voltage: ElectricalPotential){
+    fun setRollerVoltage (voltage: ElectricalPotential){
+
+    }
+    fun setWristVoltage (voltage: ElectricalPotential){
+
+    }
+    fun setFeederVoltage (voltage: ElectricalPotential){
+
+    }
+    fun setWristPosition (voltage: ElectricalPotential){
 
     }
     fun setRollerBrakeMode (brake: Boolean){
 
     }
-    fun configPID(
+    fun setFeederBrakeMode (brake: Boolean){
+
+    }
+
+
+    fun configWristPID(
         kP: ProportionalGain <Meter, Volt>,
         kI: IntegralGain <Meter, Volt>,
         kD: DerivativeGain <Meter, Volt>,
