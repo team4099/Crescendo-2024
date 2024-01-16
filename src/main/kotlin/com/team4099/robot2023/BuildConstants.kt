7bc23edb5afc91c36f4e019483b12aabb97b6947
Shom770 <shayaanwadkar@gmail.com>
2024-01-16 15:40:42 -0500

    current changes so far

--- src/main/kotlin/com/team4099/robot2023/BuildConstants.kt
+++ src/main/kotlin/com/team4099/robot2023/BuildConstants.kt
@@ -6,10 +6,10 @@ package com.team4099.robot2023
 const val MAVEN_GROUP = ""
 const val MAVEN_NAME = "Crescendo-2024"
 const val VERSION = "unspecified"
-const val GIT_REVISION = 27
-const val GIT_SHA = "e6dd2ad00da75da1686f2dc6e2688aab809838a9"
-const val GIT_DATE = "2024-01-15T20:47:37Z"
-const val GIT_BRANCH = "intake"
-const val BUILD_DATE = "2024-01-16T14:47:03Z"
-const val BUILD_UNIX_TIME = 1705434423104L
+const val GIT_REVISION = 28
+const val GIT_SHA = "eca16395eb62f3d6d367479ab32e985e6472149b"
+const val GIT_DATE = "2024-01-16T14:49:50Z"
+const val GIT_BRANCH = "targeting-command-with-drivetrain"
+const val BUILD_DATE = "2024-01-16T15:27:50Z"
+const val BUILD_UNIX_TIME = 1705436870465L
 const val DIRTY = 1
