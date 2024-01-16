39b5b6fa408c8c1014e2f127d88ecaed3e8254e9
SirBeans <146898838+SirBeans@users.noreply.github.com>
2024-01-16 15:17:56 -0500

    Fixed bugs and did more work on PID and feedforward for flywheel

--- src/main/kotlin/com/team4099/robot2023/subsystems/Shooter/Flywheel.kt
+++ src/main/kotlin/com/team4099/robot2023/subsystems/Shooter/Flywheel.kt
@@ -1,28 +1,46 @@
 package com.team4099.robot2023.subsystems.Shooter
 
+import com.ctre.phoenix6.hardware.TalonFX
+import com.ctre.phoenix6.signals.ControlModeValue
 import com.team4099.lib.hal.Clock
 import com.team4099.lib.logging.LoggedTunableValue
+import com.team4099.robot2023.config.constants.Constants
+import com.team4099.robot2023.config.constants.FlywheelConstants
+import com.team4099.robot2023.subsystems.falconspin.MotorChecker
 import org.team4099.lib.controller.SimpleMotorFeedforward
 import org.team4099.lib.units.AngularVelocity
+import org.team4099.lib.units.base.Meter
 import org.team4099.lib.units.base.seconds
-import org.team4099.lib.units.ctreAngularMechanismSensor
 import org.team4099.lib.units.derived.*
-import org.team4099.lib.units.*
+import org.team4099.lib.units.inRadiansPerSecond
+import org.team4099.lib.units.perMinute
 
 class Flywheel (val io: FlywheelIO) {
-    // TODO: Make shooter constants (kS, kV, kA)
-    var feedforward = SimpleMotorFeedforward<Volt, AngularVelocity>()
+    val motor = TalonFX(Constants.Shooter.FLYWHEEL_MOTOR_ID)
+    private val kP =
+        LoggedTunableValue("Flywheel/kP", FlywheelConstants.SHOOTER_FLYWHEEL_KP)
+    private val kI =
+        LoggedTunableValue(
+            "Flywheel/kI", FlywheelConstants.SHOOTER_FLYWHEEL_KI)
+    private val kD =
+        LoggedTunableValue(
+            "Flywheel/kD", FlywheelConstants.SHOOTER_FLYWHEEL_KD)
+
+
     val inputs = FlywheelIO.FlywheelIOInputs()
     private val flywheelkS =
-        LoggedTunableValue("Flywheel/kS", Pair({ it.inVoltsPerInch }, { it.volts.perInch }))
-    private val flywheelkkV =
+        LoggedTunableValue("Flywheel/kS", Pair({ it.inVoltsPerRadian }, { it.volts.perRadian })
+        )
+    private val flywheelkV =
         LoggedTunableValue(
-            "Flywheel/kV", Pair({ it.inVoltsPerInchSeconds }, { it.volts.perInchSeconds })
+            "Flywheel/kV", Pair({ it.inVoltsPerRadianSeconds }, { it.volts.perRadianSeconds })
         )
     private val flywheelkA =
         LoggedTunableValue(
-            "Flywheel/kA", Pair({ it.inVoltsPerInchPerSecond}, { it.volts.perInchPerSecond })
+            "Flywheel/kA", Pair({ it.inVoltsPerRadianPerSecond}, { it.volts.perRadianPerSecond })
         )
+    val flywheelFeedForward = SimpleMotorFeedforward<Radian, Volt>(flywheelkS, flywheelkV, flywheelkA)
+
 
     var flywheelTargetVoltage: ElectricalPotential = 0.0.volts
     fun setFlywheelVoltage(appliedVoltage: ElectricalPotential) {
@@ -32,7 +50,11 @@ class Flywheel (val io: FlywheelIO) {
     private var lastFlywheelVoltage = 0.0.volts
     private var flywheelInitVoltage  = LoggedTunableValue ("Shooter/Initial flywheel Voltage", FlywheelConstants.FLYWHEEEL_INIT_VOLTAGE, Pair({it.inVolts}, {it.volts}))
     private var hasNote:Boolean = true
+    val desiredVelocity: AngularVelocity = 1800.rotations.perMinute
     var currentState = Flywheel.Companion.FlywheelStates.UNINITIALIZED
+init{
+    io.configPID(FlywheelConstants.SHOOTER_FLYWHEEL_KP, FlywheelConstants.SHOOTER_FLYWHEEL_KI, FlywheelConstants.SHOOTER_FLYWHEEL_KD)
+}
     fun periodic(){
         io.updateInputs(inputs)
         var nextState = currentState
@@ -46,9 +68,9 @@ class Flywheel (val io: FlywheelIO) {
             }
             Flywheel.Companion.FlywheelStates.TARGETING_VELOCITY ->{
                 if (flywheelTargetVoltage != lastFlywheelVoltage){
-                    if(hasNote){
-
-                    }
+                    val controlEffort: ElectricalPotential = flywheelFeedForward.calculate(desiredVelocity)
+                    io.setFlywheelVelocity()//TODO talk to anshi ab a current velocity var
+                    io.setFlywheelVoltage(controlEffort)
                 }
             }
             Flywheel.Companion.FlywheelStates.ZERO ->{
