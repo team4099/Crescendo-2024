f309b3580063da9d9cc3fc9993e3d7e4e3747967
lakelandspark <bowtamer111@gmail.com>
2024-01-20 19:00:45 -0500

    Completed FeederIO

--- src/main/kotlin/com/team4099/robot2023/subsystems/feeder/FeederIO.kt
+++ src/main/kotlin/com/team4099/robot2023/subsystems/feeder/FeederIO.kt
@@ -1,17 +1,18 @@
 package com.team4099.robot2023.subsystems.feeder
 
+import com.team4099.robot2023.subsystems.superstructure.Request
 import org.littletonrobotics.junction.LogTable
 import org.littletonrobotics.junction.inputs.LoggableInputs
+import org.team4099.lib.units.*
 import org.team4099.lib.units.base.amps
 import org.team4099.lib.units.base.celsius
 import org.team4099.lib.units.base.inAmperes
 import org.team4099.lib.units.base.inCelsius
-import org.team4099.lib.units.derived.ElectricalPotential
-import org.team4099.lib.units.derived.inVolts
-import org.team4099.lib.units.derived.volts
+import org.team4099.lib.units.derived.*
 
 interface FeederIO {
     class FeederIOInputs: LoggableInputs {
+        var feederVelocity = 0.0.rotations.perMinute
         var feederAppliedVoltage = 0.0.volts
         var feederStatorCurrent = 0.0.amps
         var feederSupplyCurrent = 0.0.amps
@@ -20,6 +21,7 @@ interface FeederIO {
         var isSimulated = false
 
         override fun toLog(table: LogTable?) {
+            table?.put("feederVelocity", feederVelocity.inRadiansPerSecond)
             table?.put("feederAppliedVoltage", feederAppliedVoltage.inVolts)
             table?.put("feederStatorCurrent", feederStatorCurrent.inAmperes)
             table?.put("feederSupplyCurrent", feederSupplyCurrent.inAmperes)
@@ -27,6 +29,10 @@ interface FeederIO {
         }
 
         override fun fromLog(table: LogTable?) {
+            table?.get("feederVelocity", feederVelocity.inRadiansPerSecond)?.let {
+                feederVelocity = it.radians.perSecond
+            }
+
             table?.get("feederAppliedVoltage", feederAppliedVoltage.inVolts)?.let {
                 feederAppliedVoltage = it.volts
             }
@@ -48,6 +54,12 @@ interface FeederIO {
     fun updateInputs(inputs: FeederIOInputs) {}
 
     fun setFeederVoltage(voltage: ElectricalPotential) {}
+
+    fun setFeederVelocity(velocity: AngularVelocity, feedforward: ElectricalPotential) {}
+
+    fun setFeederBrakeMode(brake: Boolean) {}
+
+    fun configPID(kP: ProportionalGain<Velocity<Radian>, Volt>, kI: IntegralGain<Velocity<Radian>, Volt>, kD: DerivativeGain<Velocity<Radian>, Volt>) {}
 
     // fun setFloorVoltage(voltage: ElectricalPotential) {}
 
