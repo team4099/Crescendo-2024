d6c746a635e7674614f1d407d5105e09c8caf0e2
00magikarp <94652654+00magikarp@users.noreply.github.com>
2024-07-05 12:25:35 -0400

    new method of obtaining applied voltage in SwerveModuleIOTalon

--- src/main/kotlin/com/team4099/robot2023/subsystems/drivetrain/swervemodule/SwerveModuleIOTalon.kt
+++ src/main/kotlin/com/team4099/robot2023/subsystems/drivetrain/swervemodule/SwerveModuleIOTalon.kt
@@ -212,8 +212,10 @@ class SwerveModuleIOTalon(
   override fun updateInputs(inputs: SwerveModuleIO.SwerveModuleIOInputs) {
     updateSignals()
 
-    inputs.driveAppliedVoltage = (driveFalcon.get() * RobotController.getBatteryVoltage()).volts
-    inputs.steerAppliedVoltage = (steeringFalcon.get() * RobotController.getBatteryVoltage()).volts
+    inputs.driveAppliedVoltage = driveFalcon.motorVoltage.value.volts
+    inputs.steerAppliedVoltage = steeringFalcon.motorVoltage.value.volts
+    //inputs.driveAppliedVoltage = (driveFalcon.get() * RobotController.getBatteryVoltage()).volts
+    //inputs.steerAppliedVoltage = (steeringFalcon.get() * RobotController.getBatteryVoltage()).volts
 
     inputs.statorCurrentDrive = driveStatorCurrentSignal.value.amps
     inputs.supplyCurrentDrive = driveSupplyCurrentSignal.value.amps
