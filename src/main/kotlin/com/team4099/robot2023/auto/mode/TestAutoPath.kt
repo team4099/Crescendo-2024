a3544b8b8ccecdba6f5527d8586f12ee4c5cb3e7
Shom770 <shayaanwadkar@gmail.com>
2024-08-12 18:45:53 -0400

    working integration

--- src/main/kotlin/com/team4099/robot2023/auto/mode/TestAutoPath.kt
+++ src/main/kotlin/com/team4099/robot2023/auto/mode/TestAutoPath.kt
@@ -5,6 +5,7 @@ import com.team4099.lib.trajectory.FieldWaypoint
 import com.team4099.robot2023.commands.drivetrain.DrivePathCommand
 import com.team4099.robot2023.subsystems.drivetrain.drive.Drivetrain
 import com.team4099.robot2023.subsystems.superstructure.Superstructure
+import com.team4099.robot2023.util.CustomTrajectory
 import com.team4099.robot2023.util.FrameType
 import com.team4099.robot2023.util.TrajectoryTypes
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
@@ -13,6 +14,7 @@ import org.team4099.lib.geometry.Translation2d
 import org.team4099.lib.units.base.meters
 import org.team4099.lib.units.derived.degrees
 import org.team4099.lib.units.derived.inRotation2ds
+import org.team4099.lib.units.derived.radians
 
 class TestAutoPath(val drivetrain: Drivetrain, val superstructure: Superstructure) :
   SequentialCommandGroup() {
@@ -23,6 +25,26 @@ class TestAutoPath(val drivetrain: Drivetrain, val superstructure: Superstructur
       DrivePathCommand.createPathInFieldFrame(
         drivetrain,
         TrajectoryTypes.Choreo(Choreo.getTrajectory("testPath")),
+      ),
+      DrivePathCommand.createPathInFieldFrame(
+        drivetrain,
+        CustomTrajectory.fromWaypoints(
+          drivetrain,
+          {
+            listOf(
+              FieldWaypoint(
+                Translation2d(8.29.meters, 0.733.meters).translation2d,
+                null,
+                1.571.radians.inRotation2ds
+              ),
+              FieldWaypoint(
+                Translation2d(8.29.meters, 0.733.meters + 4.meters).translation2d,
+                null,
+                1.571.radians.inRotation2ds
+              )
+            )
+          }
+        )
       )
     )
   }
